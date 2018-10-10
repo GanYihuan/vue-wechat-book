71610a05f77df699bc46fa0927fadee8f2dbb9b3
GanYihuan <583520052@qq.com>
2018-10-11 04:25:35 +0800

    未上线版本1.0.0

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 ﻿// 配置项, 因为上线要换成 `上线环境部署`
 // 开发环境部署
-// const host = 'http://localhost:5757'
+const host = 'http://localhost:5757'
 // 上线环境部署
-const host = 'https://qod21e5e.qcloud.la'
+// const host = 'https://qod21e5e.qcloud.la'
 const config = {
   host,
   loginUrl: `${host}/weapp/login`,
