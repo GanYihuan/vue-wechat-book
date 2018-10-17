0073c1e3afed7552669d27fb366e58547c0eafb2
GanYihuan <583520052@qq.com>
2018-10-17 11:48:41 +0800

    v2.0.0 开发版本

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 ﻿/* 配置项, 因为上线要换成 `上线环境部署` */
 /* 开发环境部署 */
-// const host = 'http://localhost:5757'
+const host = 'http://localhost:5757'
 /* 上线环境部署 */
 // const host = 'https://qod21e5e.qcloud.la'
 /* 生产环境 */
-const host = 'https://906012892.imoocinterview.xyz'
+// const host = 'https://906012892.imoocinterview.xyz'
 const config = {
   host,
   loginUrl: `${host}/weapp/login`,
